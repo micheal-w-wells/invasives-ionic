0b0f6d75c080240c48db6a1786f1db005ec2c47d
Micheal Wells <micheal.w.wells@gov.bc.ca>
2020-09-08 10:23:12 -0700

    ionic react app with rxdb + sqlite

---
+++ new_ionic_react/photo-gallery/src/pages/Tab1.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+import {  createRxDatabase, addRxPlugin } from 'rxdb';
+                                                  // create databas
+
+
+addRxPlugin(require('pouchdb-adapter-cordova-sqlite'));
+const database =  createRxDatabase({
+    name: 'mydatabase',
+    adapter: 'cordova-sqlite' // the name of your adapter
+});
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 1 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
